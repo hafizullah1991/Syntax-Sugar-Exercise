195d6b1179e174f2c6244b4f7f7b33d6d370fce2
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-16 10:34:26 -0800

    Syntax-and-Syntax-Sugar-Exercise

---
+++ sugarexercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sugarexercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
